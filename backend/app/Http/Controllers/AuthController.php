c9b32f675f20a301971bc1e92627dcde8718075b
UCSC <sms@ucsc.cmb.ac.lk>
2018-08-31 11:39:40 +0530

    Email edited

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -54,7 +54,7 @@ class AuthController extends Controller
             'password'  => 'required'
         ]);
         // Find the user by email
-        $user = User::where('email', 'rwyman@weissnat.com')->first();
+        $user = User::where('email', $this->request->input('email'))->first();
         if (!$user) {
             // You wil probably have some sort of helpers or whatever
             // to make sure that you have the same response format for
